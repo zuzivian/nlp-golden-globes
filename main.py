36762b427d49fdf3a3229627d48ebdd3ea1e8ef1
Nathaniel Wong <NathanielWongKinYew2019@u.northwestern.edu>
2019-01-24 18:04:59 -0600

    starter code

---
+++ main.py
@@ -0,0 +1,29 @@
+import nltk
+import json
+from pprint import pprint
+import os
+
+# load json data
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(dir_path + '/data/gg2013.json') as f:
+    data = json.load(f)
+
+counter = 0
+common_words = {}
+
+for tweet in data:
+    pprint(tweet['text'])
+    counter+=1
+    words_in_tweet = tweet['text'].split()
+    for word in words_in_tweet:
+        if word in common_words.keys():
+            common_words[word] += 1
+        else:
+            common_words[word] = 1
+    if counter > 100:
+        break
+
+d_view = [ (v,k) for k,v in common_words.iteritems() ]
+d_view.sort(reverse=True) # natively sort tuples by first element
+for v,k in d_view:
+    pprint("%s: %d" % (k,v))

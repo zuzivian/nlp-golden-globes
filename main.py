9600208a80fbd202475e9ebaccec05e9ece486f4
Nathaniel Wong <NathanielWongKinYew2019@u.northwestern.edu>
2019-01-28 14:10:58 -0600

    ideas

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ with open(dir_path + '/data/gg2013.json') as f:
 counter = 0
 common_words = {}
 
+
+# common words
 for tweet in data:
     pprint(tweet['text'])
     counter+=1
@@ -22,6 +24,28 @@ for tweet in data:
             common_words[word] = 1
     if counter > 100:
         break
+
+
+# Ideas to work on, feel free to choose one and explore nltk
+
+# tokenize
+
+# group similar words? ie spelling errors, slang
+
+# noise reduction
+
+# stop lists
+
+# punctuation
+
+# n-grams
+
+# tags?
+
+# check capitalization
+
+# tfidf
+
 
 d_view = [ (v,k) for k,v in common_words.iteritems() ]
 d_view.sort(reverse=True) # natively sort tuples by first element

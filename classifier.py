d1a8c41fc3963ae982d2b2d6dd9f98d1b915a5f7
Nathaniel Wong <NathanielWongKinYew2019@u.northwestern.edu>
2019-01-29 01:40:57 -0600

    minor updates to classifier

--- classifier.py
+++ classifier.py
@@ -33,42 +33,42 @@ gg2013_categories = [
         u"Cecil B. deMille Award",
     ]
 
-gg2015_categories = {
-        "Best Motion Picture – Drama",
-        "Best Motion Picture – Musical or Comedy",
-        "Best Motion Picture – Foreign Language",
-        "Best Motion Picture – Animated",
-        "Best Director – Motion Picture"
-        "Best Actor – Motion Picture Drama",
-        "Best Actor – Motion Picture Musical or Comedy",
-        "Best Actress – Motion Picture Drama",
-        "Best Actress – Motion Picture Musical or Comedy",
-        "Best Supporting Actor – Motion Picture",
-        "Best Supporting Actress – Motion Picture",
-        "Best Screenplay – Motion Picture",
-        "Best Original Score – Motion Picture",
-        "Best Original Song – Motion Picture",
+gg2015_categories = [
+        "Best Motion Picture - Drama",
+        "Best Motion Picture - Musical or Comedy",
+        "Best Motion Picture - Foreign Language",
+        "Best Motion Picture - Animated",
+        "Best Director - Motion Picture"
+        "Best Actor - Motion Picture Drama",
+        "Best Actor - Motion Picture Musical or Comedy",
+        "Best Actress - Motion Picture Drama",
+        "Best Actress - Motion Picture Musical or Comedy",
+        "Best Supporting Actor - Motion Picture",
+        "Best Supporting Actress - Motion Picture",
+        "Best Screenplay - Motion Picture",
+        "Best Original Score - Motion Picture",
+        "Best Original Song - Motion Picture",
         "Cecil B. DeMille Award for Lifetime Achievement in Motion Pictures",
-        "Best Television Series – Drama",
-        "Best Television Series – Musical or Comedy",
+        "Best Television Series - Drama",
+        "Best Television Series - Musical or Comedy",
         "Best Miniseries or Television Film",
-        "Best Actor – Television Series Drama",
-        "Best Actor – Television Series Musical or Comedy",
-        "Best Actor – Miniseries or Television Film",
-        "Best Actress – Television Series Drama",
-        "Best Actress – Television Series Musical or Comedy",
-        "Best Actress – Miniseries or Television Film",
-        "Best Supporting Actor – Series, Miniseries or Television Film",
-        "Best Supporting Actress – Series, Miniseries or Television Film",
+        "Best Actor - Television Series Drama",
+        "Best Actor - Television Series Musical or Comedy",
+        "Best Actor - Miniseries or Television Film",
+        "Best Actress - Television Series Drama",
+        "Best Actress - Television Series Musical or Comedy",
+        "Best Actress - Miniseries or Television Film",
+        "Best Supporting Actor - Series, Miniseries or Television Film",
+        "Best Supporting Actress - Series, Miniseries or Television Film",
         "Best Documentary Film",
         "Best English-Language Foreign Film",
-        "New Star of the Year – Actor",
-        "New Star of the Year – Actress",
-        "Henrietta Award (World Film Favorite – Female)",
-        "Henrietta Award (World Film Favorite – Male)",
+        "New Star of the Year - Actor",
+        "New Star of the Year - Actress",
+        "Henrietta Award (World Film Favorite - Female)",
+        "Henrietta Award (World Film Favorite - Male)",
         "Best Film Promoting International Understanding",
         "Golden Globe Award for Best Cinematography",
-        }
+        ]
 
 
 def award_classifier(tweet_tokens, award_categories, token_dict, bigram_dict, trigram_dict):
@@ -81,7 +81,7 @@ def award_classifier(tweet_tokens, award_categories, token_dict, bigram_dict, tr
         bigram_matches = num_matches(bigram_dict[award], tweet_bigrams)
         trigram_matches = num_matches(trigram_dict[award], tweet_trigrams)
         score = 2 * token_matches + 2 * bigram_matches + 2 * trigram_matches
-        if score > best_score and score > 12:
+        if score > best_score and score > 15:
             best_score = score
             best_category = award
     return best_category

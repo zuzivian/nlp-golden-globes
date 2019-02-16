d8d37d07843127cc102552c8e0c3c4bd98a1d247
Nathaniel Wong <NathanielWongKinYew2019@u.northwestern.edu>
2019-02-16 16:00:15 -0600

    Update awardNew.py

--- submission/awardNew.py
+++ submission/awardNew.py
@@ -5,20 +5,9 @@ import re
 from nltk.corpus import stopwords
 
 stop = stopwords.words('english')
-namesdic={}
-
-
 result={}
 
-def IsInblacklist(blacklist,l):
-	for i in blacklist:
-		if i in l:
-			return True
-	return False
-
-
 def RemovePunctuation(line):
-
 	s="!#$%&()*+,-./:;<=>?@[\]^_`{'~}"
 	s+='"1234567890'
 	for x in s:
@@ -27,7 +16,6 @@ def RemovePunctuation(line):
 
 
 def EndAndStart(tweet,startlist,endlist):
-
 	for i in startlist:
 		for j in endlist:
 			l=tweet.lower().split()
@@ -38,42 +26,25 @@ def EndAndStart(tweet,startlist,endlist):
 					txt=l[temp1:temp2+1]
 					if '-' in txt:
 						if len(l[temp1:temp2+1])>=4:
-							key1=" ".join(i for i in l[temp1:temp2+1])
+							key1=" ".join(i for i in txt)
 							if key1 not in result:
 								result[key1]=1
 							else:
 								result[key1]+=1
 
-
 def getawards(year):
 	#Both json file has been tested and can successfully obtained the result
-	j_file=open('gg'+year+'.json')
-	#j_file=open('gg2015.json')
+	j_file=open('gg%s.json' % year)
 	j_str=j_file.read()
 	j_data=json.loads(j_str)
 
 	for i in j_data:
-		# if 'olden' not in i['text']:
-			EndAndStart(i['text'],['best'],['picture','tv','drama','musical','animated'])
+		EndAndStart(i['text'],['best'],['picture','tv','drama','musical','animated'])
 
-	temp = sorted(result.items(), key=lambda x: x[1], reverse=True)[0:50]
+	temp = sorted(result.items(), key=lambda x: x[1], reverse=True)[0:26]
 	res=[]
 	for i in temp:
-		if len(i[0].split())<=3:
-			continue
-		if '-' in i[0]:
+		#if i[0].split()[-1] == 'picture' or i[0].split()[-1] == 'tv':
+		res.append(RemovePunctuation(i[0].replace('tv','television')))
 
-			res.append(i[0].replace('tv','television'))
-		elif i[0].split()[-1]=='picture'or i[0].split()[-1]=='tv':
-
-			res.append(i[0].replace('tv','television'))
-
-		else:
-			continue
-
-	for i in range(0,len(res)):
-		res[i]=RemovePunctuation(res[i])
-
-	print(res[0:26])
-	return res[0:26]
-
+	return res
